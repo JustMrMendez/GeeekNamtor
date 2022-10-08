16e5e4ac3cdf3cd451c4cd73893d5192b6fe317e
Mr Mendez <mrmendez@amvestor.com>
2022-10-07 20:35:48 -0400

    added js generator to iterate log 10 urls at the time

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// domain name generator
 let pronoun = ["the", "our", "my", "your", "his", "her", "their"];
 let adj = ["great", "big", "small", "tall", "short", "long", "fat"];
 let noun = ["jogger", "racoon", "dog", "cat", "mouse", "bird", "fish"];
 let ext = [".com", ".net", ".us", ".io"];
 
-// function to generate a random domain name
 function generateDomainName() {
   return (
+    "https://" +
     pronoun[Math.floor(Math.random() * pronoun.length)] +
     adj[Math.floor(Math.random() * adj.length)] +
     noun[Math.floor(Math.random() * noun.length)] +
@@ -21,7 +19,35 @@ function generateDomainName() {
   );
 }
 
+let domainNames = [];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < ext.length; l++) {
+        domainNames.push("https://" + pronoun[i] + adj[j] + noun[k] + ext[l]);
+      }
+    }
+  }
+}
+
+// Js generator function
+function* domainLoger() {
+  let i = 0;
+  while (i < domainNames.length) {
+    yield console.table(domainNames.splice(i, i + 10));
+    i++;
+  }
+}
+
+const domainLogerGenerator = domainLoger();
+
+console.log(generateDomainName());
+console.table(domainNames.slice(0, 10));
+console.log("Total number of domains: " + domainNames.length);
+
 document.querySelector("#domain-name").innerHTML = generateDomainName();
-window.onload = function() {
-  console.log(generateDomainName());
-};
+
+document.querySelector("#generate-link").addEventListener("click", function() {
+  document.querySelector("#domain-name").innerHTML = generateDomainName();
+  console.log(domainLogerGenerator.next().value);
+});
